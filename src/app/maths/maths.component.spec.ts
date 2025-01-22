6544e7bf6d51d4e5e5999f706492d1335de1d3ec
arun <arun@ceroen.com>
2025-01-22 13:52:28 +0530

    Solved

--- src/app/maths/maths.component.spec.ts
+++ src/app/maths/maths.component.spec.ts
@@ -5,30 +5,41 @@
 
 import { Mathservices } from "./maths.component"
 
-fdescribe("Mathservices",()=>{
+describe("Mathservices",()=>{
 //check the exstence is  created
 //check add 2 number
+
+
+let service:Mathservices;
+beforeEach(() => {
+     service = new Mathservices();
+})
+
+it("should see the service INstance",()=>{
+    expect(service).toBeTruthy();  
+})
+
 it("Should add two number",()=>{
 
-    const service = new Mathservices();
-    const result = service.add(10,30);
+
+const result = service.add(10,30);
     expect(result).toEqual(40);
 })
 it("Should subtract two number",()=>{
 
-    const service = new Mathservices();
+    
     const result = service.subtract(30,30);
     expect(result).toEqual(0);
 })
 it("Should Multiply two number",()=>{
 
-    const service = new Mathservices();
+   
     const result = service.multiply(10,30);
     expect(result).toEqual(300);
 })
 it("Should Division two number",()=>{
 
-    const service = new Mathservices();
+ 
     const result = service.divide(200,10);
     expect(result).toEqual(20 );
 })
