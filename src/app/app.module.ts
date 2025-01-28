490b99287e506ea53300078daed1d40356859140
arun <arun@ceroen.com>
2025-01-28 16:21:37 +0530

    chabges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ComponentComponent } from './componentt/component/component.component';
 import { ClickResultComponent } from './componentt/click-result/click-result.component';
+import { CoursesComponent } from './courses.component';
 
 const routes: Routes = [
   // Define your routes here
 ];
 
 @NgModule({
-  declarations: [AppComponent, ComponentComponent, ClickResultComponent],
+  declarations: [AppComponent, ComponentComponent, ClickResultComponent,CoursesComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)], 
   bootstrap: [AppComponent]
 })
