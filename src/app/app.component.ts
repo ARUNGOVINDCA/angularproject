ff22976cdaa78bc3d863499566c3d91faa9f979c
arun <arun@ceroen.com>
2025-01-30 12:16:32 +0530

    nG Switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-courses: any;
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
-  greeting: string = 'Welcome to the Increment Button Demo!';
-  count: number = 0;
+    title(title: any) {
+      throw new Error('Method not implemented.');
+    }
+// courses: any;
+//   title(title: any) {
+//     throw new Error('Method not implemented.');
+//   }
+//   greeting: string = 'Welcome to the Increment Button Demo!';
+//   count: number = 0;
 
-  increment() {
-    this.count++;
-}
+//   increment() {
+//     this.count++;
+// }
+
+
+
+    vieweMode = 'Map';                                            ////Ng Switch program
+    // vieweMode = 'Something Else';    
+    
+
+    
+   
+
+
+
 }
 
